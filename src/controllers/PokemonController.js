f135da76c89bded6f350ec352899639d271e2641
Charles Marques <charles_sjp@yahoo.com.br>
2022-05-08 02:56:09 -0300

    ok

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -3,37 +3,70 @@ const Uec = require("../models/Uec");
 const getAll = async (req, res) => {
   try {
     const uecs = await Uec.findAll();
-    res.render("index", { uecs, pokemon: undefined });
+    res.render("index", {uecs});
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
 };
 
 const register = (req, res) => {
-  try{
+  try {
     res.render("register");
-  }catch (err) {
+  } catch (err) {
     res.status(500).send({ err: err.message });
   }
-}
-
-const create = async(req, res) => {
-  try{
+};
+const create = async (req, res) => {
+  try {
     const uec = req.body;
-    if(!uec){
-      return res.redirect("/register")
+    if (!uec) {
+      return res.redirect("/register");
     }
-
     await Uec.create(uec);
-    res.redirect("/")
-
-  }catch(err) {
+    res.redirect("/");
+  } catch (err) {
     res.status(500).send({ err: err.message });
   }
-}
+};
+
+const details = async (req, res) => {
+  try {
+    const uec = await Uec.findByPk(req.params.id);
+    res.render("details", {uec, uecPut: null,
+      uecDel:null})
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const method = req.params.method;
+    const uecs = await Uec.findAll();
+    const uec = await Uec.findByPk(req.params.id);
+
+    if (method == "put") {
+      res.render("index", {
+        uecs,
+        uecPut: uec,
+        uecDel: null,
+      });
+    } else {
+      res.render("index", {
+        uecs,
+        uecPut: null,
+        uecDel: uec,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 
 module.exports = {
-    getAll,
-    register,
-    create,
+  getAll,
+  create,
+  register,
+  details,
+  getById,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,7 @@ const PokemonController = require("../controllers/PokemonController");
 routes.get("/", PokemonController.getAll);
 routes.get("/register", PokemonController.register);
 routes.post("/create", PokemonController.create);
+routes.get("/details/:id", PokemonController.details)
+routes.get("/getById/:id/:method", PokemonController.getById)
 
 module.exports = routes;
